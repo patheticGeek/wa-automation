02b47ff4d678fbe964b042b64672f8c88581940c
patheticGeek <geekpathetic@gmail.com>
2020-11-24 21:42:18 +0530

    lessons learnt

--- index.js
+++ index.js
@@ -4,18 +4,18 @@ const { default: PQueue } = require("p-queue");
 const fs = require("fs");
 const express = require("express");
 
-const helpOn = ["hello", "hi", "hii", "hey", "heyy", "#help", "#menu"];
+const helpOnInPM = ["hello", "hi", "hii", "hey", "heyy", "#help", "#menu"];
+const helpOnInGroup = ["#help", "#menu"];
 
 const helpText =
   process.env.HELP_TEXT ||
   `Commands:
-#sticker: caption a image/video/gif to turn it into sticker
+#sticker: write in caption of a image/video/gif to turn it into sticker
 #spam: tag everyone in a message in a group
-#spam {n}: spam n number of times
-#join {link}: joing a group with invite link provided
-#google {query}: reply to a message or text with query to help a idot
+#join https://chat.whatsapp.com/shdkashdh: joing a group with invite link
 #leave: i hope you dont use this if you do make sure youre admin
 #help: To recive this same message
+#menu: Same as help but some people prefer it
 
 Put '#nospam' in group description to stop spam commands
 Made by: pathetic_geek (https://github.com/patheticGeek)
@@ -28,7 +28,7 @@ const leaveText =
 const server = express();
 const PORT = parseInt(process.env.PORT) || 3000;
 const queue = new PQueue({
-  concurrency: 4,
+  concurrency: 2,
   autoStart: false,
 });
 /**
@@ -43,18 +43,19 @@ let cl = null;
  */
 async function procMess(message) {
   if (message.type === "chat") {
-    if (helpOn.includes(message.body.toLowerCase())) {
+    if (
+      helpOnInGroup.includes(message.body.toLowerCase()) ||
+      helpOnInPM.includes(message.body.toLowerCase())
+    ) {
       await cl.sendText(message.from, helpText);
-    } else if (message.body.startsWith("#spam")) {
+    } else if (message.body === "#spam") {
       if (
         message.chat.groupMetadata.desc &&
         message.chat.groupMetadata.desc.includes("#nospam")
       ) {
         await cl.sendText(message.chatId, "Spam protected group");
       } else {
-        const text = `@${
-          message.author.split("@")[0]
-        } says hello ${message.chat.groupMetadata.participants.map(
+        const text = `hello ${message.chat.groupMetadata.participants.map(
           (participant) =>
             `\n🌚 @${
               typeof participant.id === "string"
@@ -62,34 +63,8 @@ async function procMess(message) {
                 : participant.user
             }`
         )}`;
-        if (message.body === "#spam") {
-          await cl.sendTextWithMentions(message.chatId, text);
-        } else {
-          const n = parseInt(message.body.replace("#spam ", ""));
-          if (n < 20) {
-            const messages = [];
-            for (let i = 0; i < n; i++) {
-              messages.push(
-                await cl.sendTextWithMentions(message.chatId, text)
-              );
-            }
-            await Promise.all(messages);
-          } else {
-            await cl.sendText(message.chatId, "Spam limit 20");
-          }
-        }
+        await cl.sendTextWithMentions(message.chatId, text);
       }
-    } else if (message.body.startsWith("#google")) {
-      const query =
-        message.body === "#google" && message.quotedMsgObj
-          ? message.quotedMsgObj.body
-          : message.body.replace("#google ", "");
-      await cl.sendText(
-        message.chatId,
-        `Here's something that'll help https://www.google.com/search?q=${encodeURIComponent(
-          query
-        )}`
-      );
     } else if (message.body.startsWith("#join https://chat.whatsapp.com/")) {
       await cl.joinGroupViaLink(message.body);
       await cl.sendText(message.chatId, "Joined group");
@@ -118,7 +93,6 @@ async function procMess(message) {
         (await cl.sendImageAsSticker(message.chatId, dataUrl));
       message.type === "video" &&
         (await cl.sendMp4AsSticker(message.chatId, dataUrl));
-      await cl.sendText(message.chatId, "Here is your sticker");
     } catch (e) {
       await cl.sendText(
         message.chatId,
